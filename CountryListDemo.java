9ff8efdca045d164cff819dc8790a9dcf9b15f65
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 16:15:17 +0530

    Add files via upload

---
+++ CountryListDemo.java
@@ -0,0 +1,55 @@
+package swingg;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+
+public class CountryListDemo extends JFrame {
+
+   
+	JList<String> countryList;
+
+    public CountryListDemo() {
+
+        setTitle("Country List");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        // List of countries
+        String[] countries = {
+            "USA", "India", "Vietnam", "Canada", "Denmark",
+            "France", "Great Britain", "Japan", "Africa",
+            "Greenland", "Singapore"
+        };
+
+        // Create JList
+        countryList = new JList<>(countries);
+
+        // Allow single selection
+        countryList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        // Add selection listener
+        countryList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+
+                // Avoid duplicate events
+                if (!e.getValueIsAdjusting()) {
+
+                    String selected = countryList.getSelectedValue();
+
+                    // Print selected country to console
+                    System.out.println("Selected Country: " + selected);
+                }
+            }
+        });
+
+        // Add scroll pane (important for JList)
+        add(new JScrollPane(countryList));
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CountryListDemo();
+    }
+}
