79052c7e2835cf6747cf77d744d8505a47c5b975
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:28:49 +0530

    Add files via upload

---
+++ p8.java
@@ -0,0 +1,20 @@
+package linkedList;
+import java.util.*;
+public class p8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<String> ls = new LinkedList<>();
+		ls.add("Orange");
+		ls.add("green");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println(ls);
+		ls.addLast("pink");
+		System.out.println(ls);
+	
+	}
+
+}
