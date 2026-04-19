358481c788ef931cca7fb0840c90dc7f6464ec0b
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:07:52 +0530

    Add files via upload

---
+++ p3.java
@@ -0,0 +1,20 @@
+package linkedList;
+import java.util.*;
+public class p3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("green");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println("Before sorting:");
+		System.out.println(ls);
+		Collections.sort(ls);
+		System.out.println("After sorting:");
+		System.out.println(ls);
+	}
+
+}
