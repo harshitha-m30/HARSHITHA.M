7a9de52dc62bdc601ddd6bb955444dde451bbbd3
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:30:17 +0530

    Update p8.java

--- p8.java
+++ p8.java
@@ -1,3 +1,6 @@
+//3. Write a Java program to insert the specified element at the end of a linked list.( using
+//l_listobj.offerLast(&quot;Pink&quot;))
+
 package linkedList;
 import java.util.*;
 public class p8 {
