5f0bb10ab49ded7b0a7d096abb74056baf7b0e0e
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:03:24 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,20 @@
+package linkedList;
+import java.util.*;
+public class p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("green");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println(ls);
+		ls.remove(2);
+		System.out.println(ls);
+		ls.remove("Blue");
+		System.out.println(ls);
+	}
+
+}
