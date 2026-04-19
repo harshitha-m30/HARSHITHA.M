5cb977297e616544c2dbf6aebbed87f33e924e6b
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:09:38 +0530

    Update p2.java

--- p2.java
+++ p2.java
@@ -1,3 +1,7 @@
+//2. Write a java program for getting different colors through ArrayList interface and remove the
+//2nd element and color &quot;Blue&quot; from the ArrayList
+
+
 package linkedList;
 import java.util.*;
 public class p2 {
