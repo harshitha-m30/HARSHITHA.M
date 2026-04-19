7f2d664965ed58fee489ee286672d7ceb4e3526c
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 14:51:24 +0530

    Update s1.java

--- s1.java
+++ s1.java
@@ -1,3 +1,15 @@
+//Q1. Write a Java Program for Checking if a given string is null or contains only whitespace using user
+//defined function isNullOrEmpty().
+//Q2. Write a Java Program for Counting how many times a substring appears in a main string
+//using user defined function countOccurrences()
+//Q3. Write a Java Program for Reversing the characters in a string using user defined function
+//reverseString().
+//Q4. Write a Java Program for Checking if a string reads the same backward as forward (ignoring case
+//and punctuation) using user defined function isPalindrome():
+//Q5. Write a Java Program for Eliminating all whitespace characters from a string using user defined
+//function removeWhitespace()
+
+
 package string;
 public class s1 {
     // 1. Function to check if a string is empty or null
