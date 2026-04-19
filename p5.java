131c2f8e559b7c84b80662628179bb4b58943132
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:19:02 +0530

    Update p5.java

--- p5.java
+++ p5.java
@@ -1,3 +1,6 @@
+//5. Write a java program for getting different colors through ArrayList interface and delete nth
+//element from the ArrayList object by using remove by index
+
 package linkedList;
 import java.util.*;
 public class p5 {
