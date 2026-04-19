34c9182a59e8bb71faf3c2d0429c8b83cbb9f041
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:37:36 +0530

    Update p10.java

--- p10.java
+++ p10.java
@@ -1,3 +1,6 @@
+//5. Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+//Collections.swap(l_list, 0, 2))
+
 package linkedList;
 import java.util.*;
 public class p10 {
