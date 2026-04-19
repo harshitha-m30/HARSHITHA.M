93011fb32be72b8720722ebe9324c1d724c1c42d
HARSHITHA.M <harshitha.mnaik30@gmail.com>
2026-04-19 12:15:01 +0530

    Update p4.java

--- p4.java
+++ p4.java
@@ -1,3 +1,6 @@
+//4. Write a java program for getting different colors through ArrayList interface and extract the
+//elements 1st and 2nd from the ArrayList object by using SubList()
+
 package linkedList;
 import java.util.*;
 public class p4 {
